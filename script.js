a849fd50b185eeec1985f6725697cfaf198fd861
adamjflynn <87862136+adamjflynn@users.noreply.github.com>
2021-10-01 12:18:41 -0500

    add weather API fetch

---
+++ script.js
@@ -0,0 +1,15 @@
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=minneapolis&appid=13262dbca390ca4dac33875e934250c9"
+
+
+fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        // pass response data to dom function
+        console.log(data)
+      });
+    }
+    else {
+      alert("There was a problem with your request!");
+    }
+  });
